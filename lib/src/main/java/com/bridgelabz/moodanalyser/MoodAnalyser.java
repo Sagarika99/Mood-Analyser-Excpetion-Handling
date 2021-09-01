058e8a2669393c532d2ab7fda8d73a762fba5726
Sagarika Shinde <sagarika.shinde@morningstar.com>
2021-09-01 21:59:41 +0530

    Ability  to analyse and respond happy or sad mood

--- lib/src/main/java/com/bridgelabz/moodanalyser/MoodAnalyser.java
+++ lib/src/main/java/com/bridgelabz/moodanalyser/MoodAnalyser.java
@@ -2,4 +2,13 @@ package com.bridgelabz.moodanalyser;
 
 public class MoodAnalyser {
 
+	public String analyseMood(String message) {
+		if(message.contains("Happy")) {
+			return "happy";
+		}
+		else if (message.contains("Sad")){
+			return "sad";
+		}
+		return "happy";
+	}
 }
--- lib/src/test/java/com/bridgelabz/moodanalysertest/MoodAnalyserTest.java
+++ lib/src/test/java/com/bridgelabz/moodanalysertest/MoodAnalyserTest.java
@@ -1,5 +1,30 @@
 package com.bridgelabz.moodanalysertest;
 
-public class MoodAnalyserTest {
+import static org.junit.Assert.assertEquals;
+import org.junit.Assert;
+import org.junit.Test;
+import com.bridgelabz.moodanalyser.MoodAnalyser;
 
+public class MoodAnalyserTest {
+	
+	@Test
+	public void givenMood_WhenHappy_ShouldReturnHappy() {
+		MoodAnalyser moodanalyser = new MoodAnalyser();
+		String mood = moodanalyser.analyseMood("I am in happy mood");
+		Assert.assertEquals("happy",mood);
+	}
+	
+	@Test
+	public void givenMood_WhenSad_ShouldReturnSad() {
+		MoodAnalyser moodanalyser = new MoodAnalyser();
+		String mood = moodanalyser.analyseMood("I am in Sad mood");
+		Assert.assertEquals("sad",mood);
+	}
+	
+	@Test
+	public void givenMood_WhenAnyMood_ShouldReturnSad() {
+		MoodAnalyser moodanalyser = new MoodAnalyser();
+		String mood = moodanalyser.analyseMood("I am in any mood");
+		Assert.assertEquals("happy",mood);
+	}
 }
